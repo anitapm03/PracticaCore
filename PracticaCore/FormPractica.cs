f665ede5d6f5e0cc89cca6d373628384366b7b1a
anitapm03 <apereiramolinero@gmail.com>
2024-01-30 13:44:37 +0100

    Add project files.

---
+++ PracticaCore/FormPractica.cs
@@ -0,0 +1,133 @@
+﻿using PracticaCore.Models;
+using PracticaCore.Repository;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+#region procedimientos
+/*
+ para seleccionar los clientes
+CREATE PROCEDURE SP_CLIENTES 
+AS
+	SELECT * FROM clientes
+GO
+
+para seleccionar los pedidos de un cliente
+ CREATE PROCEDURE SP_PEDIDOS_CLIENTE
+(@CODCLI NVARCHAR(10))
+AS
+	SELECT * FROM pedidos WHERE CodigoCliente = @CODCLI
+GO
+ 
+ para seleccionar un pedido por su codigo
+CREATE PROCEDURE SP_PEDIDO
+(@CODPED NVARCHAR(50))
+AS
+	SELECT * FROM pedidos WHERE CodigoPedido = @CODPED
+GO
+
+insertar un pedido
+CREATE PROCEDURE SP_INSERTAR_PEDIDO
+(@EMPRESA NVARCHAR(30),
+@CODPED NVARCHAR(30),
+@FECHA NVARCHAR(30),
+@FORMA NVARCHAR(30),
+@IMPORTE INT
+)
+AS
+	DECLARE @CODLI NVARCHAR(30)
+	SELECT @CODLI = CodigoCliente FROM clientes
+	WHERE Empresa = @EMPRESA
+
+	INSERT INTO pedidos VALUES (@CODPED, @CODLI,
+	@FECHA, @FORMA, @IMPORTE)
+	PRINT 'INSERTADO CORRECTAMENTE'
+GO
+ */
+#endregion
+namespace PracticaCore
+{
+    public partial class FormPractica : Form
+    {
+        RepositoryClientes repo;
+        public FormPractica()
+        {
+            InitializeComponent();
+
+            this.repo = new RepositoryClientes();
+            this.loadClientes();
+        }
+
+        private void loadClientes()
+        {
+            List<string> clientes = this.repo.GetClientes();
+            foreach (string cliente in clientes)
+            {
+                this.cmbclientes.Items.Add(cliente);
+            }
+        }
+
+        private void cmbclientes_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string empresaCliente = this.cmbclientes.SelectedItem.ToString();
+
+            Cliente cliente = this.repo.GetCliente(empresaCliente);
+
+            this.txtempresa.Text = cliente.Empresa;
+            this.txtcontacto.Text = cliente.Contacto;
+            this.txtcargo.Text = cliente.Cargo;
+            this.txtciudad.Text = cliente.Ciudad;
+            this.txttelefono.Text = cliente.Telefono;
+            string codigoCliente = cliente.CodigoCliente;
+
+            this.loadPedidosCliente(codigoCliente);
+        }
+
+        private void loadPedidosCliente(string codigoCliente)
+        {
+            List<string> pedidos = this.repo.GetPedidosCliente(codigoCliente);
+            this.lstpedidos.Items.Clear();
+            foreach (string pedido in pedidos)
+            {
+                this.lstpedidos.Items.Add(pedido);
+            }
+        }
+
+        private void lstpedidos_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string codigoPedido = this.lstpedidos.SelectedItem.ToString();
+
+            Pedido pedido = this.repo.GetInfoPedido(codigoPedido);
+
+            this.txtcodigopedido.Text = pedido.CodigoPedido;
+            this.txtfechaentrega.Text = pedido.FechaEntrega;
+            this.txtformaenvio.Text = pedido.FormaEnvio;
+            this.txtimporte.Text = pedido.Importe.ToString();
+
+        }
+
+        private void btnnuevopedido_Click(object sender, EventArgs e)
+        {
+            string empresaCliente = this.cmbclientes.SelectedItem.ToString();
+            string codigoPedido = this.txtcodigopedido.Text;
+            string fecha = this.txtfechaentrega.Text;
+            string forma = this.txtformaenvio.Text;
+            int importe = int.Parse(this.txtimporte.Text);
+
+            int insertado = this.repo.InsertarPedido(empresaCliente, codigoPedido,
+                fecha, forma, importe);
+
+            MessageBox.Show("Insertados: " + insertado);
+        }
+
+        private void btneliminarpedido_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ PracticaCore/Repository/RepositoryClientes.cs
@@ -0,0 +1,160 @@
+﻿using PracticaCore.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticaCore.Repository
+{
+    public class RepositoryClientes
+    {
+        SqlConnection cn;
+        SqlCommand com;
+        SqlDataReader reader;
+        //List<Cliente> listaClientes;
+
+        public RepositoryClientes()
+        {
+            string connectionString = "Data Source=LOCALHOST\\SQLEXPRESS;Initial Catalog=NETCORE;Persist Security Info=True;User ID=SA;Password=MCSD2023";
+            this.cn = new SqlConnection(connectionString);
+            this.com = new SqlCommand();
+            this.com.Connection = this.cn;
+            //this.listaClientes = new List<Cliente>();
+        }
+
+        public List<string> GetClientes()
+        {
+            this.com.CommandType = CommandType.StoredProcedure;
+            this.com.CommandText = "SP_CLIENTES";
+
+            this.cn.Open();
+            this.reader = this.com.ExecuteReader();
+            List<string> clientes = new List<string>();
+            while (this.reader.Read())
+            {
+                clientes.Add(this.reader["EMPRESA"].ToString());
+            }
+            this.reader.Close();
+            this.cn.Close();
+
+            return clientes;
+        }
+
+        public Cliente GetCliente(string empresaCliente)
+        {
+            string sql = "SELECT * FROM clientes WHERE Empresa =  @empresa";
+            SqlParameter empresaparam = new SqlParameter("@empresa", empresaCliente);
+            this.com.Parameters.Add(empresaparam);
+
+            this.com.CommandType = CommandType.Text;
+            this.com.CommandText = sql;
+
+            this.cn.Open();
+            this.reader = this.com.ExecuteReader();
+            
+            this.reader.Read();
+            
+                string codigo = this.reader["CodigoCliente"].ToString();
+                string empresa = this.reader["Empresa"].ToString();
+                string contacto = this.reader["Contacto"].ToString();
+                string cargo = this.reader["Cargo"].ToString();
+                string ciudad = this.reader["Ciudad"].ToString();
+                string telf = this.reader["Telefono"].ToString();
+
+                Cliente cliente = new Cliente();
+                cliente.CodigoCliente = codigo;
+                cliente.Empresa = empresa;
+                cliente.Contacto = contacto;
+                cliente.Cargo = cargo;
+                cliente.Ciudad = ciudad;
+                cliente.Telefono = telf;
+            
+            this.reader.Close();
+            this.cn.Close();
+            this.com.Parameters.Clear();
+
+            return cliente;
+        }
+
+        public List<string> GetPedidosCliente(string codCliente)
+        {
+            this.com.CommandType = CommandType.StoredProcedure;
+            this.com.CommandText = "SP_PEDIDOS_CLIENTE";
+
+            SqlParameter parCod = new SqlParameter("@CODCLI", codCliente);
+            this.com.Parameters.Add(parCod);
+
+            this.cn.Open();
+            this.reader = this.com.ExecuteReader();
+            List<string> pedidos = new List<string>();
+            while (this.reader.Read())
+            {
+                pedidos.Add(this.reader["CodigoPedido"].ToString());
+            }
+            this.reader.Close();
+            this.cn.Close();
+            this.com.Parameters.Clear();
+            return pedidos;
+        }
+
+        public Pedido GetInfoPedido(string codPedido)
+        {
+            this.com.CommandType = CommandType.StoredProcedure;
+            this.com.CommandText = "SP_PEDIDO";
+
+            SqlParameter parCod = new SqlParameter("@CODPED", codPedido);
+            this.com.Parameters.Add(parCod);
+
+            this.cn.Open();
+            this.reader = this.com.ExecuteReader();
+
+            this.reader.Read();
+            string codigo = this.reader["CodigoPedido"].ToString();
+            string cliente = this.reader["CodigoCliente"].ToString();
+            string fecha = this.reader["FechaEntrega"].ToString();
+            string forma = this.reader["FormaEnvio"].ToString();
+            int importe = int.Parse(this.reader["Importe"].ToString());
+
+            Pedido pedido = new Pedido();
+            pedido.CodigoPedido = codigo;
+            pedido.CodigoCliente = cliente;
+            pedido.FechaEntrega = fecha;
+            pedido.FormaEnvio = forma;
+            pedido.Importe = importe;
+
+            this.reader.Close();
+            this.cn.Close();
+            this.com.Parameters.Clear();
+            return pedido;
+        }
+
+        public int InsertarPedido
+            (string empresa, string codigopedido, string fecha, string forma, int importe)
+        {
+            
+            SqlParameter parEmpresa = new SqlParameter("@EMPRESA", empresa);
+            this.com.Parameters.Add(parEmpresa);
+            SqlParameter parCodigo = new SqlParameter("@CODPED", codigopedido);
+            this.com.Parameters.Add(parCodigo);
+            SqlParameter parFecha = new SqlParameter("@FECHA", fecha);
+            this.com.Parameters.Add(parFecha);
+            SqlParameter parForma = new SqlParameter("@FORMA", forma);
+            this.com.Parameters.Add(parForma);
+            SqlParameter parImporte = new SqlParameter("@IMPORTE", importe);
+            this.com.Parameters.Add(parImporte);
+
+            this.com.CommandType = CommandType.StoredProcedure;
+            this.com.CommandText = "SP_INSERTAR_PEDIDO";
+
+            this.cn.Open();
+            int insertado = this.com.ExecuteNonQuery();
+
+            this.cn.Close();
+            this.com.Parameters.Clear();
+            return insertado;
+        }
+    }
+}
